ee89c877d3b34db294cf998567df28f426ce8cc6
erickdalton <daltonerick17@gmail.com>
2024-09-24 09:39:28 +0700

    halooo

--- project/backend/actions/authCheck.php
+++ project/backend/actions/authCheck.php
@@ -3,4 +3,4 @@ if(!isset($_SESSION['id'])){
     $_SESSION['error'] = "Anda tidak memiliki akses karena tidak Login!";
     echo "<script>window.location.href = '../pages/login.php'</script>";
     exit();
-}
+}//haloooooo
